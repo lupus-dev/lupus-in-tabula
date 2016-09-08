5e51cf840f78156c4de09cf0927f319a4a6651a3
Edoardo Morassutto <gianluigi988@gmail.com>
2016-09-08 09:31:04 +0200

    Now the test suite can be run all together

--- tests/features/common/common.js
+++ tests/features/common/common.js
@@ -1,4 +1,8 @@
 module.exports = function() {
+	if (global.common_included)
+		return;
+	global.common_included = true;
+
 	var $this = this;
 	require('fs').readdirSync(__dirname + '/step_definitions/').forEach(function(file) {
 		if (file.match(/\.js$/) !== null) {
