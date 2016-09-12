f1f9ea0d96d08bc80738975d7d91a4dc48aabb70
Edoardo Morassutto <gianluigi988@gmail.com>
2016-09-12 16:04:46 +0200

    Fixed links in the navbar

--- lupus-frontend/app/shared/navbar/navbar.component.ts
+++ lupus-frontend/app/shared/navbar/navbar.component.ts
@@ -1,7 +1,14 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
 	selector: 'lupus-navbar',
 	templateUrl: 'app/shared/navbar/navbar.component.html'
 })
-export class NavbarComponent { }
+export class NavbarComponent {
+	constructor(public router: Router) { }
+
+	isActiveRoute(route: string) : boolean {
+		return this.router.url == route;
+	}
+}
