83101cf6d19a27314c258cd3971fdceb94efb4fb
Edoardo Morassutto <gianluigi988@gmail.com>
2016-09-12 21:42:53 +0200

    Added signup button

--- lupus-frontend/app/shared/navbar/navbar.component.ts
+++ lupus-frontend/app/shared/navbar/navbar.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
+import { SessionService } from '../session.service';
+
 @Component({
 	selector: 'lupus-navbar',
 	templateUrl: 'app/shared/navbar/navbar.component.html'
 })
 export class NavbarComponent {
-	constructor(public router: Router) { }
+	constructor(public router: Router,
+				public sessionService: SessionService) { }
+
+	isLoggedIn(): boolean {
+		return !!this.sessionService.session;
+	}
 
 	isActiveRoute(route: string) : boolean {
 		return this.router.url == route;
