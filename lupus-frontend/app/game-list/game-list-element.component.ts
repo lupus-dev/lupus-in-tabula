86e8e0238a16b386bb963e1e02596a9963a5925a
Edoardo Morassutto <gianluigi988@gmail.com>
2016-09-13 15:04:53 +0200

    Now game list elements are colored

--- lupus-frontend/app/game-list/game-list-element.component.ts
+++ lupus-frontend/app/game-list/game-list-element.component.ts
@@ -8,4 +8,18 @@ import { Game } from './game.model';
 })
 export class GameListElementComponent {
 	@Input() game: Game;
+
+	gameLabel(): string {
+		let code = this.game.state.status.code;
+		switch (code) {
+			case 'draft':   return 'panel-default';
+			case 'open':    return 'panel-primary';
+			case 'closed':  return 'panel-warning';
+			case 'full':    return 'panel-warning';
+			case 'running': return 'panel-success';
+			case 'ended':   return 'panel-info';
+			case 'stopped': return 'panel-danger';
+		}
+		return 'panel-danger';
+	}
 }
