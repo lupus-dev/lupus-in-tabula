9a2f636534a06c43c906407dea9a63b5432bfb49
Edoardo Morassutto <gianluigi988@gmail.com>
2016-10-27 22:52:07 +0200

    Again...

--- lupus-game/game-logic/engine.js
+++ lupus-game/game-logic/engine.js
@@ -67,7 +67,7 @@ module.exports = class Engine {
 
 		if (this.game.members[0]+'' != this.game.state.players[0].user_id+'' ||
 			this.game.members[1]+'' != this.game.state.players[1].user_id+'')
-			console.error("--------------- ERROR #1", require('util').inspect(this.game, { depth: null }));
+			debug("--------------- ERROR #1", require('util').inspect(this.game, { depth: null }));
 
 		this.game.state.votes = [];
 		this._setupRoles();
@@ -75,7 +75,7 @@ module.exports = class Engine {
 
 		if (this.game.members[0]+'' != this.game.state.players[0].user_id+'' ||
 			this.game.members[1]+'' != this.game.state.players[1].user_id+'')
-			console.error("--------------- ERROR #2", require('util').inspect(this.game, { depth: null }));
+			debug("--------------- ERROR #2", require('util').inspect(this.game, { depth: null }));
 
 		// update the random state from the engine
 		this.game.gen_info.random.useCount = engine.getUseCount();
@@ -84,22 +84,22 @@ module.exports = class Engine {
 
 		if (this.game.members[0]+'' != this.game.state.players[0].user_id+'' ||
 			this.game.members[1]+'' != this.game.state.players[1].user_id+'')
-			console.error("--------------- ERROR #3", require('util').inspect(this.game, { depth: null }));
+			debug("--------------- ERROR #3", require('util').inspect(this.game, { depth: null }));
 
 		return this.game.save()
 			.then(game => {
 				if (game.members[0]+'' != game.state.players[0].user_id+'' ||
 					game.members[1]+'' != game.state.players[1].user_id+'')
-					console.error("--------------- ERROR #4", require('util').inspect(game, { depth: null }));
+					debug("--------------- ERROR #4", require('util').inspect(game, { depth: null }));
 			});
 
 		setTimeout(() => {
 			Game.findOne({ _id: game_id })
 				.then(game => {
-					console.log('#########' + require('util').inspect(game, { depth: null }));
+					debug('#########' + require('util').inspect(game, { depth: null }));
 					if (game.members[0]+'' != game.state.players[0].user_id+'' ||
 						game.members[1]+'' != game.state.players[1].user_id+'')
-						console.error("--------------- ERROR #5", require('util').inspect(game, { depth: null }));
+						debug("--------------- ERROR #5", require('util').inspect(game, { depth: null }));
 				});
 		}, 1000);
 	}
